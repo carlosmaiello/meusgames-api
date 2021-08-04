510cc3362ab93cc64704aeb2e314684342dae020
carlosmaiello <carlosmaiello@gmail.com>
2021-08-04 18:45:57 -0300

    Autenticação

--- meusgames/settings.py
+++ meusgames/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_jwt',
     'games',
 ]
 
@@ -126,5 +127,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
 
 django_heroku.settings(locals())
--- meusgames/urls.py
+++ meusgames/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from games.api.urls import router as router_games
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router_games.urls)),
-
+# https://medium.com/@hudsonbrendon/autentica%C3%A7%C3%A3o-com-jwt-no-django-rest-framework-45626936c276
+    url(r'^login/', obtain_jwt_token),
+    url(r'^refresh-token/', refresh_jwt_token),
 ]
